e5d2b94c2e0962f2cf57ae6bdbc09144ba3bf30a
Lisa <lisa.djaghri@laplateforme.io>
2025-09-30 11:32:00 +0200

    job04

---
+++ jour03/job04/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$str = "Dans l'espace, personne ne vous entend crier";
+
+$compteur = 0;
+
+while (isset($str[$compteur])) {
+    $compteur++;
+}
+
+echo "Nombre de caractères : " . $compteur;
+?>
