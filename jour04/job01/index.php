6329de5438884e5a7b2da99bfdee365e5a78c896
Lisa <lisa.djaghri@laplateforme.io>
2025-10-02 11:39:31 +0200

    job02

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -1,6 +1,7 @@
 <html>
 <form method= "GET">
-    <input type= "text" name= "text">
+    <input type= "text" name= "text1">
+    <input type= "text" name= "text2">
     <input type= "submit">
 </form>
 </html>
