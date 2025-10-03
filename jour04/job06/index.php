7d2bc5059e2145afa52f3f6bdb3fc33cf67f3152
Lisa <lisa.djaghri@laplateforme.io>
2025-10-03 14:42:01 +0200

    job06

---
+++ jour04/job06/index.php
@@ -0,0 +1,15 @@
+<html>
+<form method="GET">
+    <input type="text" name="nombre">
+    <input type="submit" value="Envoyer">
+</form>
+
+<?php
+if ($_GET["nombre"] % 2 == 0) {
+    echo "Nombre pair";
+}
+else {
+    echo "Nombre impair";
+}
+
+?>
