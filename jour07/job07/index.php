5e853db19c4e67499cc504e01d88104768ae93ee
Lisa <lisa.djaghri@laplateforme.io>
2025-10-10 11:40:23 +0200

    J7J7

--- jour07/job07/index.php
+++ jour07/job07/index.php
@@ -49,21 +49,104 @@ function gras($str) {
     return $resultat;
 }
     
+function cesar($str, $decalage = 2) {
+    $alphabetMin = "abcdefghijklmnopqrstuvwxyz";
+    $alphabetMaj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $texteFinal = "";
+    $positionLettre = 0;
+
+    while (isset($str[$positionLettre])) {
+        $caractereActuel = $str[$positionLettre];
+        $estUneLettre = false;
+
+        $positionDansAlphabet = 0;
+        while (isset($alphabetMin[$positionDansAlphabet])) {
+            if ($alphabetMin[$positionDansAlphabet] == $caractereActuel) {
+                $nouvellePosition = ($positionDansAlphabet + $decalage) % 26;
+                $caractereActuel = $alphabetMin[$nouvellePosition];
+                $estUneLettre = true;
+                break;
+            }
+            $positionDansAlphabet++;
+        }
+
+        if (!$estUneLettre) {
+            $positionDansAlphabet = 0;
+            while (isset($alphabetMaj[$positionDansAlphabet])) {
+                if ($alphabetMaj[$positionDansAlphabet] == $caractereActuel) {
+                    $nouvellePosition = ($positionDansAlphabet + $decalage) % 26;
+                    $caractereActuel = $alphabetMaj[$nouvellePosition];
+                    break;
+                }
+                $positionDansAlphabet++;
+            }
+        }
+
+        $texteFinal .= $caractereActuel;
+        $positionLettre++;
+    }
+
+    return $texteFinal;
+}
+
+function plateforme($str) {
+    $motActuel = "";
+    $texteFinal = "";
+    $positionCaractere = 0;
+
+    while (isset($str[$positionCaractere])) {
+        $caractereActuel = $str[$positionCaractere];
+
+        if ($caractereActuel == " ") {
+
+            $tailleMot = 0;
+            while (isset($motActuel[$tailleMot])) {
+                $tailleMot++;
+            }
+
+            if ($tailleMot >= 2 && $motActuel[$tailleMot - 2] == 'm' && $motActuel[$tailleMot - 1] == 'e') {
+                $motActuel .= "_";
+            }
+
+            $texteFinal .= $motActuel . " ";
+            $motActuel = "";
+        } else {
+
+            $motActuel .= $caractereActuel;
+        }
+
+        $positionCaractere++;
+    }
+
+    $tailleMot = 0;
+    while (isset($motActuel[$tailleMot])) {
+        $tailleMot++;
+    }
+
+    if ($tailleMot >= 2 && $motActuel[$tailleMot - 2] == 'm' && $motActuel[$tailleMot - 1] == 'e') {
+        $motActuel .= "_";
+    }
+
+    $texteFinal .= $motActuel;
+
+    return $texteFinal;
+}
+
     if (isset($_POST["str"]) && isset($_POST["fonction"])) {
         $str = $_POST["str"];
         $fonction = $_POST["fonction"];
         
         if ($fonction == "gras") {
-            echo gras($str);
+           echo gras($str);
         }
         elseif ($fonction == "cesar") {
-            echo "Fonction cesar à faire";
+            echo cesar($str);
         }
         elseif ($fonction == "plateforme") {
-            echo "Fonction plateforme à faire";
+            echo plateforme($str);
         }
     }
-    
+
     ?>
 </body>
 </html>
